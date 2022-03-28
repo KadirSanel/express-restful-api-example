8fa0790a56f1208fc4a2e86a275ca42f257094cc
KadirSanel <sanelkadir@gmail.com>
2022-03-28 19:39:39 +0300

    First commit with completing auth feature

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// requirements are defined
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+// require routes
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+
+// starting appS
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 5000 // port
+
+// connect to atlas cluster
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Connection to atlas cluster is succsessfull"))
+    .catch((err) => {console.log(err)})
+
+// route
+app.use(express.json())
+app.use("/api/v1/auth", authRoute)
+app.use("/api/v1/users", userRoute)
+
+// start service and listen
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
